f1bd6c1bda07569b036896ab7bba8d4398ee228b
Kazim Zaidi <kazimzaidi2010@gmail.com>
2026-09-03 11:27:14 -0400

    original commit

--- StudentTypeDriver.cpp
+++ StudentTypeDriver.cpp
@@ -8,39 +8,37 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	StudentType student1, student2, student3;		// Student Type contains PersonType(name, DateType DOB) and enrollment status
-	DateType student1DOB, student2DOB, student3DOB; // Date type contains month, day, year
+int main(){
+	StudentType student1, student2, student3;
+	DateType student1DOB, student2DOB,student3DOB;
 
-	student1DOB.Initialize(7, 15, 1978); // (Month, Day, Year): July 15, 1978
-	student2DOB.Initialize(6, 23, 1980); // June 23, 1980
-	student3DOB.Initialize(3, 4, 1945);	 // March 4, 1945
+	student1DOB.Initialize(7, 15, 1978);
+	student2DOB.Initialize(6, 23, 1980);
+	student3DOB.Initialize(3, 4, 1945);
 
-	student1.Initialize("Ivan", student1DOB, ENROLLED); // PersonType (Name, DateType, Status)
+	student1.Initialize("Ivan", student1DOB, ENROLLED);
 	student2.Initialize("Jim", student2DOB, GRADUATED);
 	student3.Initialize("Billy", student3DOB, NON_ATTENDING);
 
-	UnsortedType classList; // Creates list
+	UnsortedType classList;
 
-	classList.PutItem(student1); // classList.PutItem -> Adds item to list
+	classList.PutItem(student1);
 	classList.PutItem(student2);
-	// classList.PutItem(student3);
 
-	// NON_ATTENDING = 0, ENROLLED = 1, GRADUATED = 2
-	//  cout << "Compare student1 to student1: " << student1.ComparedTo(student1) << endl;  //expect 1 for same status // Enrolled == Enrolled
-	//  cout << "Compare student1 to student2: " << student1.ComparedTo(student2) << endl;  //expect 0 when status is not the same // Enrolled != Graduated
+	//NON_ATTENDING = 0, ENROLLED = 1, GRADUATED = 2
+	cout << "Compare student1 to student1: " << student1.ComparedTo(student1) << endl;  //expect 1 for same status
+	cout << "Compare student1 to student2: " << student1.ComparedTo(student2) << endl;  //expect 0 when status is not the same
 
-	cout << "Print list 0:\n"; // student1, student2
-	classList.ResetList();	   // reset the current position to NULL
+	cout << "Print list 0:\n";
+	classList.ResetList();  //reset the current position to NULL
 	classList.Print();
-	cout << "Print list 1:\n"; // student2
+	cout << "Print list 1:\n";
 	classList.DeleteItem(student1);
-	classList.ResetList(); // this must be reset before printing the list everytime
+	classList.ResetList();  //this must be reset before printing the list everytime
 	classList.Print();
-	cout << "Print list 2:\n"; // student2 and student3
+	cout << "Print list 2:\n";
 	classList.PutItem(student3);
-	classList.ResetList(); // this must be reset before printing the list everytime
+	classList.ResetList();  //this must be reset before printing the list everytime
 	classList.Print();
 	cout << "Print list 3:\n";
 	classList.DeleteItem(student2);
--- unsorted.cpp
+++ unsorted.cpp
@@ -4,36 +4,29 @@
 #include "unsorted.h"
 #include <iostream>
 using namespace std;
-struct NodeType // Node struct contains data and pointer to NEXT
+struct NodeType
 {
-  ItemType info;
-  NodeType *next;
+    ItemType info;
+    NodeType* next;
 };
 
-UnsortedType::UnsortedType() // Class constructor
+UnsortedType::UnsortedType()  // Class constructor
 {
-  // Constructs an unsorted list with length 0
-
   length = 0;
   listData = NULL;
-  head = new NodeType;
-  head->next = NULL;
-  tail = new NodeType;
-  head->next = tail; // connects head and tail–all other elements will be placed between in between
-  currentPos = cursor = head;
 }
 bool UnsortedType::IsFull() const
 // Returns true if there is no room for another ItemType
 //  on the free store; false otherwise.
 {
-  NodeType *location;
+  NodeType* location;
   try
   {
     location = new NodeType;
     delete location;
     return false;
   }
-  catch (std::bad_alloc exception)
+  catch(std::bad_alloc exception)
   {
     return true;
   }
@@ -48,159 +41,72 @@ int UnsortedType::GetLength() const
 void UnsortedType::MakeEmpty()
 // Post: List is empty; all items have been deallocated.
 {
-  NodeType *tempPtr;
+  NodeType* tempPtr;
 
-  while (listData != NULL)
-  {
-    tempPtr = listData;
-    listData = listData->next;
-    delete tempPtr;
+    while (listData != NULL)
+    {
+      tempPtr = listData;
+      listData = listData->next;
+      delete tempPtr;
   }
   length = 0;
 }
-void UnsortedType::PutItem(ItemType item) // Rewrote this from scratch
+void UnsortedType::PutItem(ItemType item)
 // item is in the list; length has been incremented.
 {
+  NodeType* location;			// Declare a pointer to a node
 
-  NodeType *newNode = new NodeType;
-  newNode->info = item;
-  NodeType *temp = head->next;
-  head->next = newNode;
-  newNode->next = temp;
-
-  length++;
+  location = new NodeType;		// Get a new node 
+  location->info = item;		// Store the item in the node
+  location->next = listData;	// Store address of first node 
+						//   in next field of new node
+  listData = location;		// Store address of new node into
+						//   external pointer
+  length++;				// Increment length of the list
 }
 
-// Came with Driver:
-
-// ItemType UnsortedType::GetItem(ItemType &item, bool &found) // ItemType is a StudenType that inherets from PersonType
-// // Pre:  Key member(s) of item is initialized.
-// // Post: If found, item's key matches an element's key in the
-// //       list and a copy of that element has been stored in item;
-// //       otherwise, item is unchanged.
-// {
-//   bool moreToSearch;
-//   NodeType *location;
-
-//   location = listData;
-//   found = false;
-//   moreToSearch = (location != NULL);
-
-//   while (moreToSearch && !found)
-//   {
-//     switch (item.ComparedTo(location->info))
-//     {
-//     case LESS:
-//     case GREATER:
-//       location = location->next;
-//       moreToSearch = (location != NULL);
-//       break;
-//     case EQUAL:
-//       found = true;
-//       item = location->info;
-//       break;
-//     }
-//   }
-//   return item;
-// }
-
-// Kazim's Version:
-
-ItemType UnsortedType::GetItem(ItemType &item, bool &found) // ItemType is a StudenType that inherets from PersonType
+ItemType UnsortedType::GetItem(ItemType& item, bool& found)
 // Pre:  Key member(s) of item is initialized.
-// Post: If found, item's key matches an element's key in the
+// Post: If found, item's key matches an element's key in the 
 //       list and a copy of that element has been stored in item;
-//       otherwise, item is unchanged.
+//       otherwise, item is unchanged. 
 {
+  bool moreToSearch;
+  NodeType* location;
 
-  ResetList(); // currentPos and cursor are at head
+  location = listData;
+  found = false;
+  moreToSearch = (location != NULL);
 
-  found = true;
-
-  for (int k = 0; k < length; k++)
+  while (moreToSearch && !found) 
   {
-    currentPos = currentPos->next;
-    while (!found)
+    switch (item.ComparedTo(location->info))
     {
-      switch (item.ComparedTo(currentPos->info))
-      {
-      case LESS:
-        break;
-      case GREATER:
-        break;
-      case EQUAL:
-        found = true;
-        break;
-      }
+      case LESS    : 
+      case GREATER : location = location->next;
+                     moreToSearch = (location != NULL);
+                     break;
+      case EQUAL   : found = true;
+                     item = location->info;
+                     break;
     }
   }
-
-  // bool moreToSearch;
-  // NodeType *location;
-
-  // location = listData;
-  // found = false;
-  // moreToSearch = (location != NULL);
-
-  // while (moreToSearch && !found)
-  // {
-  //   switch (item.ComparedTo(location->info))
-  //   {
-  //   case LESS:
-  //   case GREATER:
-  //     location = location->next;
-  //     moreToSearch = (location != NULL);
-  //     break;
-  //   case EQUAL:
-  //     found = true;
-  //     item = location->info;
-  //     break;
-  //   }
-  // }
-  // return item;
+  return item;
 }
 
-// void UnsortedType::DeleteItem(ItemType item)
-// // Pre:  item's key has been initialized.
-// //       An element in the list has a key that matches item's.
-// // Post: No element in the list has a key that matches item's.
-// {
-//   NodeType *location = listData;
-//   NodeType *tempLocation;
-
-//   // Locate node to be deleted.
-//   if (item.ComparedTo(listData->info) == EQUAL)
-//   {
-//     tempLocation = location;
-//     listData = listData->next; // Delete first node.
-//   }
-//   else
-//   {
-//     while (item.ComparedTo((location->next)->info) != EQUAL)
-//       location = location->next;
-
-//     // Delete node at location->next
-//     tempLocation = location->next;
-//     location->next = (location->next)->next;
-//   }
-//   delete tempLocation;
-//   length--;
-// }
-
-void UnsortedType::DeleteItem(ItemType item)
+ void UnsortedType::DeleteItem(ItemType item)
 // Pre:  item's key has been initialized.
 //       An element in the list has a key that matches item's.
 // Post: No element in the list has a key that matches item's.
 {
-
-  NodeType *location = listData;
-  NodeType *tempLocation;
+  NodeType* location = listData;
+  NodeType* tempLocation;
 
   // Locate node to be deleted.
   if (item.ComparedTo(listData->info) == EQUAL)
   {
     tempLocation = location;
-    listData = listData->next; // Delete first node.
+    listData = listData->next;		// Delete first node.
   }
   else
   {
@@ -218,10 +124,9 @@ void UnsortedType::DeleteItem(ItemType item)
 void UnsortedType::ResetList()
 // Post: Current position has been initialized.
 {
-  // currentPos = NULL;
-  currentPos = cursor = head;
+  currentPos = NULL;
 }
-
+ 
 ItemType UnsortedType::GetNextItem()
 // Post:  A copy of the next item in the list is returned.
 //        When the end of the list is reached, currentPos
@@ -239,7 +144,7 @@ ItemType UnsortedType::GetNextItem()
 UnsortedType::~UnsortedType()
 // Post: List is empty; all items have been deallocated.
 {
-  NodeType *tempPtr;
+  NodeType* tempPtr;
 
   while (listData != NULL)
   {
@@ -249,50 +154,10 @@ UnsortedType::~UnsortedType()
   }
 }
 
-void UnsortedType::Print()
-{
-  // ItemType item is a StudentType. listData points to the first node after each reset.
-  // -> dereferences the listData pointer so that we can access the data sitting inside this node
-
-  if (length == 0)
-  {
-    std::cout << "Empty" << std::endl;
-    return;
-  }
-  else
-  {
-    for (int k = 0; k < length; k++)
-    {
-      ItemType item = listData->info;
-      std::cout << "Name: " << item.NameIs() << std::endl;
-      std::cout << "Status: " << item.GetStatus() << std::endl;
-      listData = listData->next;
-      // Complete this.;
-    }
-  }
-  return;
+void UnsortedType::Print(){
+	//Complete this.
 }
 
-/*
-void UnsortedType::Print()
-{
-  // ItemType item is a StudentType. listData points to the first node after each reset.
-  // -> dereferences the listData pointer so that we can access the data sitting inside this node
-  if (listData->next == NULL)
-  {
-    std::cout << "Empty" << std::endl;
-  }
-  else
-  {
-    while (listData != NULL)
-    {
-      ItemType item = listData->info;
-      std::cout << "Name: " << item.NameIs() << std::endl;
-      std::cout << "Status: " << item.GetStatus() << std::endl;
-      listData = listData->next;
-      // Complete this.;
-    }
-  }
-}
-*/
-// g++ *.cpp -o main && ./main
+
+
+
